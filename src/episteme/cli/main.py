b9c3b9feb94d09a15ce2257b0aafe877328393aa
Amjad Chami <amjad.chami@gmail.com>
2025-06-04 04:37:59 -0400

    smoke test for the cli

--- src/episteme/cli/main.py
+++ src/episteme/cli/main.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser
 
 
-def get_args() -> ArgumentParser:
+def get_parser() -> ArgumentParser:
     parser = ArgumentParser()
     parser.add_argument(
         "task",
-        action="store"
+        action="store",
+        help="task to generate a plan for"
     )
 
     return parser
